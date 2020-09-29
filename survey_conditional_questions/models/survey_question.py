dfac8f224c6915ff804fc3b324fbdb0dfca4a4a1
Stefan Rijnhart <stefan@opener.am>
2020-09-29 16:30:49 +0200

    [ADD] Conditional questions on the same page

--- survey_conditional_questions/models/survey_question.py
+++ survey_conditional_questions/models/survey_question.py
@@ -8,11 +8,11 @@ from odoo import api, fields, models
 class SurveyQuestion(models.Model):
     _inherit = 'survey.question'
 
+    # Don't copy conditional fields. It could lead to references to a
+    # different survey if a survey was copied.
     is_conditional = fields.Boolean(
         'Conditional Question',
         copy=False,
-        # we add copy = false to avoid wrong link on survey copy,
-        # should be improoved
     )
     triggering_question_id = fields.Many2one(
         'survey.question',
@@ -28,16 +28,38 @@ class SurveyQuestion(models.Model):
     )
 
     @api.multi
+    def _hidden_on_same_page(self, post):
+        """ Indicate that the question was hidden dynamically due to answers
+        to questions on the same page
+        """
+        self.ensure_one()
+        qkey = '%s_%s_%s' % (self.survey_id.id, self.page_id.id,
+                             self.triggering_question_id.id)
+        if qkey in post:
+            # Compare the simple choice answer to the triggering answer
+            return str(post[qkey]) != str(self.triggering_answer_id.id)
+        akey = '%s_%s' % (qkey, self.triggering_answer_id.id)
+        if (any(key.startswith(qkey + '_') for key in post.keys())
+                and akey not in post):
+            # Multiple choice answers for the triggering question are present,
+            # but not this question's triggering answer
+            return True
+        return False
+
+    @api.multi
     def validate_question(self, post, answer_tag):
         """ Skip validation of hidden questions """
         self.ensure_one()
-        if self.triggering_question_id:
-            input_answer_ids = self.env['survey.user_input_line'].search(
-                [('user_input_id.token', '=', post.get('token')),
-                 ('question_id', '=', self.triggering_question_id.id)])
-            for answers in input_answer_ids:
-                value_suggested = answers.value_suggested
-                if (self.is_conditional and
-                        self.triggering_answer_id != value_suggested):
+        if self.is_conditional and self.triggering_question_id:
+            if self.page_id == self.triggering_question_id.page_id:
+                if self._hidden_on_same_page(post):
+                    return {}
+            else:
+                # In case the dependent question was on a previous page
+                input_answer_ids = self.env['survey.user_input_line'].search(
+                    [('user_input_id.token', '=', post.get('token')),
+                     ('question_id', '=', self.triggering_question_id.id)])
+                if self.triggering_answer_id not in input_answer_ids.mapped(
+                        'value_suggested'):
                     return {}
         return super(SurveyQuestion, self).validate_question(post, answer_tag)
--- survey_conditional_questions/static/src/js/survey_conditional_questions.js
+++ survey_conditional_questions/static/src/js/survey_conditional_questions.js
@@ -12,19 +12,65 @@ odoo.define('survey.conditional_question', function (require) {
                 function(json_data){
                     // For each of these, hide the question label and the answer
                     _.each(json_data.hidden_questions, function(key){
-                        the_form.find(".js_question-wrapper[id=" + key + "]").css("display", "none");
+                        the_form.find(".js_question-wrapper[id=" + key + "]").addClass('hidden');
                     });
                     _.each(json_data.hidden_pages, function(key){
                         var div = the_form.find("h1[data-oe-id=" + key + "][data-oe-model='survey.page']").parent();
-                        div.css("display", "none");
+                        div.addClass('hidden');
                         // Also hide the adjacent hr tag
-                        div.prev().css("display", "none");
+                        div.prev().addClass('hidden');
                     });
                 });
         }
     }
 
+    function _survey_conditional_on_change(event){
+        // Hide or show conditional questions on the same page
+        var target = event.target;
+        var value = target.value;
+        if (value == -1) {
+            return;
+        }
+        var type = target.getAttribute('type');
+        var question = target.name.split('_')[2];
+        if ($(target).is('select') || type == 'radio'){
+            the_form.find('[triggering_question_id="'+ question + '"]').each(function(index, item){
+                if (item.getAttribute('triggering_answer_id') == value){
+                    $(item).removeClass('hidden');
+                } else {
+                    $(item).addClass('hidden');
+                }
+            });
+        } else if (type == 'checkbox'){
+            the_form.find('[triggering_question_id="'+ question + '"]').each(function(index, item){
+                if (item.getAttribute('triggering_answer_id') == value){
+                    if (target.checked) {
+                        $(item).removeClass('hidden');
+                    } else {
+                        $(item).addClass('hidden');
+                    }
+                }
+            });
+        }
+    }
+
     if(the_form.length) {
+        // Hide conditional questions depending on questions on earlier pages
         hide_conditional_questions();
+        // Set onchange events on all simple and multiple choice answers
+        if (the_form.is('form')) {  // i.e. not the print layout
+            the_form.find("label > input[type='checkbox']").each(function(index, item){
+                $(item).change(_survey_conditional_on_change);
+                _survey_conditional_on_change({'target': item});
+            });
+            the_form.find("label > input[type='radio']").each(function(index, item){
+                $(item).change(_survey_conditional_on_change);
+                _survey_conditional_on_change({'target': item});
+            });
+            the_form.find("select").each(function(index, item){
+                $(item).change(_survey_conditional_on_change);
+                _survey_conditional_on_change({'target': item});
+            });
+        }
     }
 });
